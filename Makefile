2c671de93ce7ce87ec6068cea52ab97c3b965f68
root <root@www.(none)>
2014-05-04 15:34:54 +0000

    Maj des scripts d'installation

--- Makefile
+++ Makefile
@@ -21,3 +21,29 @@ clean:
 
 uninstall: clean
 	sudo apt-get autoremove --purge snack
+
+testing: clean
+	sudo wget --no-check-certificate https://github.com/bh-consulting/snack/archive/master.zip
+	sudo unzip master.zip
+	sudo mv snack-master interface
+	sudo cp -a paquet paquet_deb
+	sudo cp db/*.sql paquet_deb/tmp/snack/
+	sudo cp -R interface paquet_deb/home/snack/interface
+	sudo find paquet_deb -name .svn -exec rm -r {} +
+	sudo chmod 0440 paquet_deb/home/snack/conf/sudoers.d/snack
+	sudo chown root:root paquet_deb/home/snack/conf/sudoers.d/snack
+	sudo chmod 777 -R paquet_deb/home/snack/interface/app/tmp
+	sudo ./tools/scriptVersion.sh testing
+
+stable: clean
+	sudo wget --no-check-certificate https://github.com/bh-consulting/snack/archive/master.zip
+	sudo unzip master.zip
+	sudo mv snack-master interface
+	sudo cp -a paquet paquet_deb
+	sudo cp db/*.sql paquet_deb/tmp/snack/
+	sudo cp -R interface paquet_deb/home/snack/interface
+	sudo find paquet_deb -name .svn -exec rm -r {} +
+	sudo chmod 0440 paquet_deb/home/snack/conf/sudoers.d/snack
+	sudo chown root:root paquet_deb/home/snack/conf/sudoers.d/snack
+	sudo chmod 777 -R paquet_deb/home/snack/interface/app/tmp
+	sudo ./tools/scriptVersion.sh stable
--- tools/scriptVersion.sh
+++ tools/scriptVersion.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
-VERSION=`ls /home/www/debs/stable/snack_* | cut -d"_" -f2 | cut -d"_" -f1`
+if [ $# -lt 2 ]; then
+    release="stable"
+fi
+release=$1
+VERSION=`ls /home/www/debs/$release/snack_* | cut -d"_" -f2 | cut -d"_" -f1`
 REV=`echo $VERSION | cut -d"-" -f2`
 RAC=`echo $VERSION | cut -d"-" -f1`
 if [[ "$REV" == "$VERSION" ]]; then
@@ -19,10 +22,10 @@ whiptail \
 if [[ $? == 1 ]]; then
     NEWVERSION=$(whiptail --inputbox "Enter the version number you want?" 8 78 --title "Version Number" 3>&1 1>&2 2>&3)
 fi
-sed -e "s/Version: $VERSION/Version: $NEWVERSION/" -i paquet_deb/DEBIAN/control
+sed -e "s/Version: .*/Version: $NEWVERSION/" -i paquet_deb/DEBIAN/control
 sed -e "s/SNACK 1.0/SNACK $NEWVERSION/" -i paquet_deb/home/snack/interface/app/View/Layouts/default.ctp
-dpkg-deb --build paquet_deb "snack_"$NEWVERSION"_deb7u1_all.deb"
-mv /home/www/debs/stable/*.deb /home/www/debs/archives
-cp "snack_"$NEWVERSION"_deb7u1_all.deb" /home/www/debs/stable
-cd /home/www/debs && ./scriptRepo.sh
+#dpkg-deb --build paquet_deb "snack_"$NEWVERSION"_$release_deb7u1_all.deb"
+#mv /home/www/debs/$release/*.deb /home/www/debs/archives
+#cp "snack_"$NEWVERSION"_$release_deb7u1_all.deb" /home/www/debs/$release
+#cd /home/www/debs && ./scriptRepo.sh
 
